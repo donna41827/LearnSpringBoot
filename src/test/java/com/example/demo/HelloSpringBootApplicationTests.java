a13d0427c6a90f8bfe1f467b3748ba9f63d7aa79
donna41827 <donna41827@gmail.com>
2020-07-27 23:28:50 +0800

    YamlSetting

--- src/test/java/com/example/demo/HelloSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/HelloSpringBootApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.example.demo.bean.Person;
+/*
+ * 這是SpringBoot的單元測試(@SpringBootTest)
+*/
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class HelloSpringBootApplicationTests {
+	
+	@Autowired
+	Person person;
 
 	@Test
 	void contextLoads() {
+		System.out.println(person);
 	}
 
 }
