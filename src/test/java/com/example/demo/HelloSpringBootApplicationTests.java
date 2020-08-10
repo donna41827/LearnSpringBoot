dc94bcbccf359508b94e1a333efe58b0a47f7c9e
donna41827 <donna41827@gmail.com>
2020-08-10 21:42:14 +0800

    TryImportResource20200810

--- src/test/java/com/example/demo/HelloSpringBootApplicationTests.java
+++ src/test/java/com/example/demo/HelloSpringBootApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.example.demo.bean.Person;
@@ -25,12 +27,16 @@ class HelloSpringBootApplicationTests {
 
 	@Autowired//自動綁定
 	psPerson person3;
+	
+	@Autowired//自動綁定
+	ApplicationContext ioc;
 
 	@Test
 	void contextLoads() {
 		System.out.println("使用@ConfigurationPropertie取值:"+person);
 		System.out.println("使用@Value取值:"+person2);
 		System.out.println("使用@PropertySource取特定配置文件值:"+person3);
+		System.out.println("使用@ImportResource看是否成功載入HelloServiceBean?"+ioc.containsBean( "helloService" ));
 	}
 
 }
